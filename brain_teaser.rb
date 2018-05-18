f5bcbb3a06f011f807b2ec752ee109a4a5e18179
Alan Bischoff <alanlbischoff@gmail.com>
2018-05-18 15:11:13 -0600

    enter numbers, find the min, and max

--- brain_teaser.rb
+++ brain_teaser.rb
@@ -1,34 +1,23 @@
 def input
-  @num_arr = []
+  max = 0
+  min = 0
+  num = []
+  choice = 'y'
+  while choice ==  'y'
   puts "please enter a number"
-  set = 0
-  num = gets.to_i
-  if num <= set
-    @num_arr.unshift(num)
-  elsif num >= set
-    @num_arr << num
-  else
-    @num_arr.insert(1,num)
-  end
-  set = num
-  puts "would you like to add another number?(Y/N)"
-  answer = gets.strip.downcase
-  if answer == "y"
-    input
-  elsif answer == "n"
-    min_max
-  else
-    puts "not a valid option"
-    input
+  num << gets.to_i
+  puts "would you like to enter another number? (y/n)"
+  choice = gets.strip
+ end
+  num.each do |letter|
+    letter = letter
+  if letter >= max
+    max = letter
+  else letter <= min
+    min = letter
   end
 end
+  puts "the max number is #{max} and the lowest number is #{min}"
+end
 
-    def min_max
-      puts "The lowest and higest numbers are"
-      lowest = @num_arr.each_with_index do |num, i |
-      end
-      puts "The lowest number is #{num}"
-      highest = @num_arr.each_with_index do |high| @num_arr.length
-      end
-        puts "The highest number is #{high}"
-  end
+input
